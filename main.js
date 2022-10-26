979618d20adfd821f574c911a085eb8e92dfd7c2
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-10-26 14:37:53 +0100

    got kick drum working on any key press

---
+++ main.js
@@ -0,0 +1,11 @@
+// grab the key to use as input
+//on press, play sound
+const audio = document.getElementById('kick');
+
+window.addEventListener('keydown', (e) => {
+    playSound(e.key);
+});
+
+function playSound(key) {
+    audio.play();
+}
