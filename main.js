586e5dbd4f349f9a7993796a1fb813948e23f967
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-10-31 13:02:32 +0000

    Added functionality for playing kick sample

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 // grab the key to use as input
 //on press, play sound
+const kick = document.getElementById("kick");
 
-window.addEventListener('keydown', (e) => {
-    playSound(e.key);
-    switch (e.key) {
-        case 'q':
-            playSound();
-    }
+window.addEventListener("keydown", (e) => {
+  playSound(e.key);
 });
 
 function playSound(key) {
-    const audio = document.getElementById('kick');
-    audio.currentTime = 0;
-    audio.play();
+  switch (key) {
+    case "q":
+      kick.currentTime = 0;
+      kick.play();
+  }
 }
