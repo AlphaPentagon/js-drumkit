8abdf1fdb1e8f9816fd6adec96d1c8840ebc2394
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-10-31 13:10:13 +0000

    added audio eleents for each sample and grabbed them by id

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 // grab the key to use as input
 //on press, play sound
-const kick = document.getElementById("kick");
+const kick = document.getElementById('kick');
+const aahwwaahh = document.getElementById('aahwwaahh');
+const awwyea = document.getElementById('awwyea');
+const boww = document.getElementById('boww');
+const clap = document.getElementById('clap');
+const go = document.getElementById('go');
+const hiHat = document.getElementById('hi-hat');
+const snare = document.getElementById('snare');
+const uhh = document.getElementById('uhh');
 
-window.addEventListener("keydown", (e) => {
-  playSound(e.key);
+window.addEventListener('keydown', (e) => {
+    playSound(e.key);
 });
 
 function playSound(key) {
-  switch (key) {
-    case "q":
-      kick.currentTime = 0;
-      kick.play();
-  }
+    switch (key) {
+        case 'q':
+            kick.currentTime = 0;
+            kick.play();
+    }
 }
