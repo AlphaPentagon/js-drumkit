5534c01afbdab79e268e27d6dc1c9b7d9784fa8c
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-11-01 16:19:13 +0000

    volume knob sorted

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const audioCtx = new AudioContext();
 const volume = audioCtx.createGain();
 volume.connect(audioCtx.destination);
 
-const slider1 = document.getElementById('slider1');
-slider1.addEventListener('input', (e) => {
-  console.log(e.target.value);
-  volume.gain.value = e.target.value;
+const volumeKnob = document.getElementById('volume-knob');
+volumeKnob.addEventListener('input', (e) => {
+    console.log(e.target.value);
+    volume.gain.value = e.target.value;
 });
 
 //sounds
@@ -45,78 +45,78 @@ snareSource.connect(volume);
 uhhSource.connect(volume);
 
 window.addEventListener('keydown', (e) => {
-  if (audioCtx.state === 'suspended') {
-    audioCtx.resume();
-  }
-  playSound(e.key.toLowerCase());
+    if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
+    }
+    playSound(e.key.toLowerCase());
 });
 
 window.addEventListener('click', (e) => {
-  if (audioCtx.state === 'suspended') {
-    audioCtx.resume();
-  }
-  console.log('what was clicked:', e.target.id);
-  playSound(e.target.id);
+    if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
+    }
+    console.log('what was clicked:', e.target.id);
+    playSound(e.target.id);
 });
 
 function activateBorder(className) {
-  let pad = document.querySelector(className);
-  pad.classList.add('pressed');
-  setTimeout(() => {
-    pad.classList.remove('pressed');
-  }, 500);
+    let pad = document.querySelector(className);
+    pad.classList.add('pressed');
+    setTimeout(() => {
+        pad.classList.remove('pressed');
+    }, 500);
 }
 
 function playSound(key) {
-  switch (key) {
-    case 'q':
-      kick.currentTime = 0;
-      activateBorder('.drum-pad-1');
-      kick.play();
-      break;
-    case 'w':
-      aahwwaahh.currentTime = 0;
-      aahwwaahh.play();
-      activateBorder('.drum-pad-2');
-      break;
-    case 'e':
-      awwyea.currentTime = 0;
-      awwyea.volume = 0.3;
-      awwyea.play();
-      activateBorder('.drum-pad-3');
-      break;
-    case 'a':
-      boww.currentTime = 0;
-      boww.volume = 0.2;
-      boww.play();
-      activateBorder('.drum-pad-4');
-      break;
-    case 's':
-      clap.currentTime = 0;
-      clap.play();
-      activateBorder('.drum-pad-5');
-      break;
-    case 'd':
-      go.currentTime = 0;
-      go.volume = 0.4;
-      go.play();
-      activateBorder('.drum-pad-6');
-      break;
-    case 'z':
-      hiHat.currentTime = 0;
-      hiHat.play();
-      activateBorder('.drum-pad-7');
-      break;
-    case 'x':
-      snare.currentTime = 0;
-      snare.play();
-      activateBorder('.drum-pad-8');
-      break;
-    case 'c':
-      uhh.currentTime = 0;
-      uhh.volume = 0.3;
-      uhh.play();
-      activateBorder('.drum-pad-9');
-      break;
-  }
+    switch (key) {
+        case 'q':
+            kick.currentTime = 0;
+            activateBorder('.drum-pad-1');
+            kick.play();
+            break;
+        case 'w':
+            aahwwaahh.currentTime = 0;
+            aahwwaahh.play();
+            activateBorder('.drum-pad-2');
+            break;
+        case 'e':
+            awwyea.currentTime = 0;
+            awwyea.volume = 0.3;
+            awwyea.play();
+            activateBorder('.drum-pad-3');
+            break;
+        case 'a':
+            boww.currentTime = 0;
+            boww.volume = 0.2;
+            boww.play();
+            activateBorder('.drum-pad-4');
+            break;
+        case 's':
+            clap.currentTime = 0;
+            clap.play();
+            activateBorder('.drum-pad-5');
+            break;
+        case 'd':
+            go.currentTime = 0;
+            go.volume = 0.4;
+            go.play();
+            activateBorder('.drum-pad-6');
+            break;
+        case 'z':
+            hiHat.currentTime = 0;
+            hiHat.play();
+            activateBorder('.drum-pad-7');
+            break;
+        case 'x':
+            snare.currentTime = 0;
+            snare.play();
+            activateBorder('.drum-pad-8');
+            break;
+        case 'c':
+            uhh.currentTime = 0;
+            uhh.volume = 0.3;
+            uhh.play();
+            activateBorder('.drum-pad-9');
+            break;
+    }
 }
