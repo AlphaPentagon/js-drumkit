693f136605499fe1e1ad3b86f2f2f8ed155db40d
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-11-02 16:11:45 +0000

    Added on/off switch and linked functions

--- bootupdisplay.js
+++ bootupdisplay.js
@@ -1,59 +1,59 @@
 import { activateBorder } from './main.js';
 
-document.addEventListener('DOMContentLoaded', function () {
-    bootborder('.drum-pad-1');
-    setTimeout(function () {
-        bootborder('.drum-pad-4');
-        bootborder('.drum-pad-2');
-    }, 200);
-    setTimeout(function () {
-        bootborder('.drum-pad-7');
-        bootborder('.drum-pad-5');
-        bootborder('.drum-pad-3');
-    }, 400);
-    setTimeout(function () {
-        bootborder('.drum-pad-8');
-        bootborder('.drum-pad-6');
-    }, 600);
-    setTimeout(function () {
-        bootborder('.drum-pad-9');
-    }, 1000);
-    setTimeout(function () {
-        activateBorder('.drum-pad-9');
-    }, 1400);
-    setTimeout(function () {
-        activateBorder('.drum-pad-8');
-        activateBorder('.drum-pad-6');
-    }, 1600);
-    setTimeout(function () {
-        activateBorder('.drum-pad-7');
-        activateBorder('.drum-pad-5');
-        activateBorder('.drum-pad-3');
-    }, 1800);
-    setTimeout(function () {
-        activateBorder('.drum-pad-4');
-        activateBorder('.drum-pad-2');
-    }, 2000);
-    setTimeout(function () {
-        activateBorder('.drum-pad-1');
-    }, 2200);
-    setTimeout(function () {
-        activateBorder('.drum-pad-1');
-        activateBorder('.drum-pad-2');
-        activateBorder('.drum-pad-3');
-        activateBorder('.drum-pad-4');
-        activateBorder('.drum-pad-5');
-        activateBorder('.drum-pad-6');
-        activateBorder('.drum-pad-7');
-        activateBorder('.drum-pad-8');
-        activateBorder('.drum-pad-9');
-    }, 2800);
-});
+export function bootUpDisplay() {
+  bootborder('.drum-pad-1');
+  setTimeout(function () {
+    bootborder('.drum-pad-4');
+    bootborder('.drum-pad-2');
+  }, 200);
+  setTimeout(function () {
+    bootborder('.drum-pad-7');
+    bootborder('.drum-pad-5');
+    bootborder('.drum-pad-3');
+  }, 400);
+  setTimeout(function () {
+    bootborder('.drum-pad-8');
+    bootborder('.drum-pad-6');
+  }, 600);
+  setTimeout(function () {
+    bootborder('.drum-pad-9');
+  }, 1000);
+  setTimeout(function () {
+    activateBorder('.drum-pad-9');
+  }, 1400);
+  setTimeout(function () {
+    activateBorder('.drum-pad-8');
+    activateBorder('.drum-pad-6');
+  }, 1600);
+  setTimeout(function () {
+    activateBorder('.drum-pad-7');
+    activateBorder('.drum-pad-5');
+    activateBorder('.drum-pad-3');
+  }, 1800);
+  setTimeout(function () {
+    activateBorder('.drum-pad-4');
+    activateBorder('.drum-pad-2');
+  }, 2000);
+  setTimeout(function () {
+    activateBorder('.drum-pad-1');
+  }, 2200);
+  setTimeout(function () {
+    activateBorder('.drum-pad-1');
+    activateBorder('.drum-pad-2');
+    activateBorder('.drum-pad-3');
+    activateBorder('.drum-pad-4');
+    activateBorder('.drum-pad-5');
+    activateBorder('.drum-pad-6');
+    activateBorder('.drum-pad-7');
+    activateBorder('.drum-pad-8');
+    activateBorder('.drum-pad-9');
+  }, 2800);
+}
 
 function bootborder(className) {
-    let pad = document.querySelector(className);
-    pad.classList.add('boot-display');
-    setTimeout(() => {
-        pad.classList.remove('boot-display');
-    }, 500);
+  let pad = document.querySelector(className);
+  pad.classList.add('boot-display');
+  setTimeout(() => {
+    pad.classList.remove('boot-display');
+  }, 500);
 }
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { loadSounds, nineties } from "./loadSounds.js";
+import { loadSounds, nineties } from './loadSounds.js';
+import { bootUpDisplay } from './bootupdisplay.js';
 
 //instance of Audio API
 export const audioCtx = new AudioContext();
@@ -9,10 +10,10 @@ export const volume = audioCtx.createGain();
 //this connects the volume effect to the output (speakers)
 volume.connect(audioCtx.destination);
 //link volume knob to volume effect
-const volumeKnob = document.getElementById("volume-knob");
-volumeKnob.addEventListener("input", (e) => {
-    console.log(e.target.value);
-    volume.gain.value = e.target.value;
+const volumeKnob = document.getElementById('volume-knob');
+volumeKnob.addEventListener('input', (e) => {
+  console.log(e.target.value);
+  volume.gain.value = e.target.value;
 });
 
 //PANNING
@@ -26,15 +27,15 @@ let currentDeck = loadSounds(nineties);
 console.log(currentDeck[0]);
 
 //link panning knob to panning effect
-const panningKnob = document.getElementById("panning-knob");
-panningKnob.addEventListener("input", (e) => {
-    console.log(e.target.value);
-    panning.positionX.value = e.target.value;
+const panningKnob = document.getElementById('panning-knob');
+panningKnob.addEventListener('input', (e) => {
+  console.log(e.target.value);
+  panning.positionX.value = e.target.value;
 });
 // Double click to reset pan to neutral
-panningKnob.addEventListener("dblclick", (e) => {
-    e.target.value = 0;
-    panning.positionX.value = e.target.value;
+panningKnob.addEventListener('dblclick', (e) => {
+  e.target.value = 0;
+  panning.positionX.value = e.target.value;
 });
 
 //ANALYSER
@@ -45,116 +46,153 @@ const bufferLength = analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
 analyser.getByteTimeDomainData(dataArray);
 
-const canvas = document.getElementById("oscilloscope");
-const canvasCtx = canvas.getContext("2d");
+const canvas = document.getElementById('oscilloscope');
+const canvasCtx = canvas.getContext('2d');
+let JamOn = false;
 
 function draw() {
-    let colourArr = ["#F00","#f80","#ff0","#0F0","#0ff","#00F","#94f"]
+  let colourArr = ['#F00', '#f80', '#ff0', '#0F0', '#0ff', '#00F', '#94f'];
+  if (JamOn) {
     requestAnimationFrame(draw);
     analyser.getByteTimeDomainData(dataArray);
     // console.log({dataArray:dataArray})
-    canvasCtx.fillStyle = "rgb(0, 0, 0)";
+    canvasCtx.fillStyle = 'rgb(5, 49, 7)';
     canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
     canvasCtx.lineWidth = 2;
     // canvasCtx.strokeStyle =colourArr[Math.floor(Math.random()*colourArr.length)];
-    canvasCtx.strokeStyle = dataArray.every((e)=>{return e == 128}) ? "#fff": colourArr[Math.floor(Math.random()*colourArr.length)];
+    canvasCtx.strokeStyle = dataArray.every((e) => {
+      return e == 128;
+    })
+      ? '#fff'
+      : colourArr[Math.floor(Math.random() * colourArr.length)];
     canvasCtx.beginPath();
     const sliceWidth = (canvas.width * 1.0) / bufferLength;
     let x = 0;
     for (let i = 0; i < bufferLength; i++) {
-        const v = dataArray[i] / 128.0;
-        const y = (v * canvas.height) / 2;
-        if (i === 0) {
-            canvasCtx.moveTo(x, y);
-        } else {
-            canvasCtx.lineTo(x, y);
-        }
-        x += sliceWidth;
+      const v = dataArray[i] / 128.0;
+      const y = (v * canvas.height) / 2;
+      if (i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+      x += sliceWidth;
     }
     canvasCtx.lineTo(canvas.width, canvas.height / 2);
     canvasCtx.stroke();
+  } else {
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
-draw();
 
-window.addEventListener("keydown", (e) => {
-    // Listen for a key press on the keyboard and play the sound assocaited with that key (if valid)
-    if (audioCtx.state === "suspended") {
-        // Check that on keypress, there has been no user events on the page, and if not, then resume the audio
-        audioCtx.resume();
-    }
-    playSound(e.key.toLowerCase());
+window.addEventListener('keydown', (e) => {
+  // Listen for a key press on the keyboard and play the sound assocaited with that key (if valid)
+  if (audioCtx.state === 'suspended') {
+    // Check that on keypress, there has been no user events on the page, and if not, then resume the audio
+    audioCtx.resume();
+  }
+  playSound(e.key.toLowerCase());
 });
 
-window.addEventListener("mousedown", (e) => {
-    // Listen for a click on the page, and play the sound asssociated with the clicked element
-    if (audioCtx.state === "suspended") {
-        // Check that on keypress, there has been no user events on the page, and if not, then resume the audio
-        audioCtx.resume();
-    }
-    console.log("what was clicked:", e.target.id);
-    playSound(e.target.id);
+window.addEventListener('mousedown', (e) => {
+  // Listen for a click on the page, and play the sound asssociated with the clicked element
+  if (audioCtx.state === 'suspended') {
+    // Check that on keypress, there has been no user events on the page, and if not, then resume the audio
+    audioCtx.resume();
+  }
+  console.log('what was clicked:', e.target.id);
+  playSound(e.target.id);
 });
 
 export function activateBorder(className) {
-    // Adds the "pressed" class to the clicked element, and remove it after 500ms. The class makes the element's outline "glow" neon pink
-    let pad = document.querySelector(className);
-    pad.classList.add("pressed");
-    setTimeout(() => {
-        pad.classList.remove("pressed");
-    }, 500);
+  // Adds the "pressed" class to the clicked element, and remove it after 500ms. The class makes the element's outline "glow" neon pink
+  let pad = document.querySelector(className);
+  pad.classList.add('pressed');
+  setTimeout(() => {
+    pad.classList.remove('pressed');
+  }, 500);
 }
 
 function playSound(key) {
-    // Plays the sound corresponding to the specified key
+  // Plays the sound corresponding to the specified key
+  if (JamOn) {
     switch (key) {
-        case "q":
-            currentDeck[0].currentTime = 0;
-            currentDeck[0].play();
-            activateBorder(".drum-pad-1");
-            break;
-        case "w":
-            currentDeck[1].currentTime = 0;
-            currentDeck[1].play();
-            activateBorder(".drum-pad-2");
-            break;
-        case "e":
-            currentDeck[2].currentTime = 0;
-            currentDeck[2].volume = 0.3;
-            currentDeck[2].play();
-            activateBorder(".drum-pad-3");
-            break;
-        case "a":
-            currentDeck[3].currentTime = 0;
-            currentDeck[3].volume = 0.2;
-            currentDeck[3].play();
-            activateBorder(".drum-pad-4");
-            break;
-        case "s":
-            currentDeck[4].currentTime = 0;
-            currentDeck[4].play();
-            activateBorder(".drum-pad-5");
-            break;
-        case "d":
-            currentDeck[5].currentTime = 0;
-            currentDeck[5].volume = 0.4;
-            currentDeck[5].play();
-            activateBorder(".drum-pad-6");
-            break;
-        case "z":
-            currentDeck[6].currentTime = 0;
-            currentDeck[6].play();
-            activateBorder(".drum-pad-7");
-            break;
-        case "x":
-            currentDeck[7].currentTime = 0;
-            currentDeck[7].play();
-            activateBorder(".drum-pad-8");
-            break;
-        case "c":
-            currentDeck[8].currentTime = 0;
-            currentDeck[8].volume = 0.3;
-            currentDeck[8].play();
-            activateBorder(".drum-pad-9");
-            break;
+      case 'q':
+        currentDeck[0].currentTime = 0;
+        currentDeck[0].play();
+        activateBorder('.drum-pad-1');
+        break;
+      case 'w':
+        currentDeck[1].currentTime = 0;
+        currentDeck[1].play();
+        activateBorder('.drum-pad-2');
+        break;
+      case 'e':
+        currentDeck[2].currentTime = 0;
+        currentDeck[2].volume = 0.3;
+        currentDeck[2].play();
+        activateBorder('.drum-pad-3');
+        break;
+      case 'a':
+        currentDeck[3].currentTime = 0;
+        currentDeck[3].volume = 0.2;
+        currentDeck[3].play();
+        activateBorder('.drum-pad-4');
+        break;
+      case 's':
+        currentDeck[4].currentTime = 0;
+        currentDeck[4].play();
+        activateBorder('.drum-pad-5');
+        break;
+      case 'd':
+        currentDeck[5].currentTime = 0;
+        currentDeck[5].volume = 0.4;
+        currentDeck[5].play();
+        activateBorder('.drum-pad-6');
+        break;
+      case 'z':
+        currentDeck[6].currentTime = 0;
+        currentDeck[6].play();
+        activateBorder('.drum-pad-7');
+        break;
+      case 'x':
+        currentDeck[7].currentTime = 0;
+        currentDeck[7].play();
+        activateBorder('.drum-pad-8');
+        break;
+      case 'c':
+        currentDeck[8].currentTime = 0;
+        currentDeck[8].volume = 0.3;
+        currentDeck[8].play();
+        activateBorder('.drum-pad-9');
+        break;
     }
+  }
 }
+
+//on-LED function to light up
+const onOffSwitch = document.getElementById('on-off-switch');
+const onLED = document.getElementById('on-LED');
+const JAM = document.getElementById('jamdeck');
+const dPad = document.querySelectorAll('.drum-pad');
+onOffSwitch.addEventListener('click', (e) => {
+  if (!onLED.classList.contains('turned-on')) {
+    onLED.classList.add('turned-on');
+    JAM.classList.add('jamdeck-on');
+    bootUpDisplay();
+    JamOn = true;
+    draw();
+    console.log(dPad);
+    dPad.forEach((item) => {
+      item.classList.add('active-pad');
+    });
+  } else {
+    onLED.classList.remove('turned-on');
+    JAM.classList.remove('jamdeck-on');
+    JamOn = false;
+    dPad.forEach((item) => {
+      item.classList.remove('active-pad');
+    });
+  }
+});
