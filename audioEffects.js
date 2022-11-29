6dfba0306cb15bf67599d152d2027a952a167096
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-11-29 11:31:30 +0000

    Distortion now working

--- audioEffects.js
+++ audioEffects.js
@@ -10,57 +10,55 @@ export let pitchShift = 1;
 export let distortionShift = 0;
 
 export function connectEffectsToSources(source) {
-    source.connect(volume);
-    volume.connect(panning);
-    panning.connect(distortion);
-    distortion.connect(analyser);
-    analyser.connect(audioCtx.destination);
+  source.connect(volume);
+  volume.connect(panning);
+  panning.connect(distortion);
+  distortion.connect(analyser);
+  analyser.connect(audioCtx.destination);
 }
 
 //DISTORTION
 //used to create the distortion curve
 
 function makeDistortionCurve(amount) {
-    if (amount == 0) {
-        return;
-    }
-    const k = typeof amount === 'number' ? amount : 50;
-    const n_samples = 44100;
-    const curve = new Float32Array(n_samples);
-    const deg = Math.PI / 180;
+  if (amount == 0) {
+    return;
+  }
+  const k = typeof amount === "number" ? amount : 50;
+  const n_samples = 44100;
+  const curve = new Float32Array(n_samples);
+  const deg = Math.PI / 180;
 
-    for (let i = 0; i < n_samples; i++) {
-        const x = (i * 2) / n_samples - 1;
-        curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
-    }
-    return curve;
+  for (let i = 0; i < n_samples; i++) {
+    const x = (i * 2) / n_samples - 1;
+    curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
+  }
+  return curve;
 }
 
-const volumeKnob = document.getElementById('volume-knob');
-volumeKnob.addEventListener('input', (e) => {
-    console.log('Volume changed to: ', e.target.value);
-    volume.gain.value = e.target.value;
+const volumeKnob = document.getElementById("volume-knob");
+volumeKnob.addEventListener("input", (e) => {
+  console.log("Volume changed to: ", e.target.value);
+  volume.gain.value = e.target.value;
 });
-const pitchSlider = document.getElementById('slider1');
-pitchSlider.addEventListener('input', (e) => {
-    console.log('Pitch changed to: ', e.target.value);
-    pitchShift = e.target.value;
+const pitchSlider = document.getElementById("slider1");
+pitchSlider.addEventListener("input", (e) => {
+  console.log("Pitch changed to: ", e.target.value);
+  pitchShift = e.target.value;
 });
-const distortionSlider = document.getElementById('slider2');
-distortionSlider.addEventListener('input', (e) => {
-    console.log('Distortion changed to: ', e.target.value);
-    distortion.curve = makeDistortionCurve(e.target.value);
-
-    // distortion.oversample = '2x';
+const distortionSlider = document.getElementById("slider2");
+distortionSlider.addEventListener("input", (e) => {
+  console.log("Distortion changed to: ", e.target.value);
+  distortion.curve = makeDistortionCurve(Number(e.target.value));
 });
 
-const panningKnob = document.getElementById('panning-knob');
-panningKnob.addEventListener('input', (e) => {
-    console.log('Panning position changed to: ', e.target.value);
-    panning.positionX.value = e.target.value;
+const panningKnob = document.getElementById("panning-knob");
+panningKnob.addEventListener("input", (e) => {
+  console.log("Panning position changed to: ", e.target.value);
+  panning.positionX.value = e.target.value;
 });
-panningKnob.addEventListener('dblclick', (e) => {
-    console.log('Panning reset to middle!');
-    e.target.value = 0;
-    panning.positionX.value = e.target.value;
+panningKnob.addEventListener("dblclick", (e) => {
+  console.log("Panning reset to middle!");
+  e.target.value = 0;
+  panning.positionX.value = e.target.value;
 });
